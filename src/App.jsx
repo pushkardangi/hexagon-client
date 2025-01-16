b9ccfcd7feeaa19e4be89b50e3ddcc6a04bfee5e
Pushkar Dangi <pushkardangi746@gmail.com>
2025-01-16 15:03:55 +0530

    feat: update assets, and implement image generation form

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,15 @@ const App = () => {
     {/* header */}
       <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
 
-        <Link to="/">
-          <img src={logo} alt="logo" className="w-28 object-contain"/>
-        </Link>
+        <div className="flex items-center">
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-10 object-contain"/>
+          </Link>
+          <span className="ml-2 text-xl">Hexagon</span>
+        </div>
 
         <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
-          Create
+          Create images
         </Link>
       </header>
 
