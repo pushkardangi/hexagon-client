642a42d9dc87e027cdf5186ee85356b91840dc32
Pushkar Dangi <pushkardangi746@gmail.com>
2025-02-20 10:27:53 +0530

    refactor: Radio component to remove unused disable prop and improve code readability

--- src/components/Radio.jsx
+++ src/components/Radio.jsx
@@ -1,11 +1,16 @@
-const Radio = ({ name, value, handleChange, selectedValue, disable }) => {
+const Radio = ({ name, value, handleChange, selectedValue }) => {
+
   const label = value.charAt(0).toUpperCase() + value.slice(1);
   const isSelected = selectedValue === value; // Check if this option is selected
 
   return (
     <label
       className={`flex items-center justify-center w-24 px-1 py-0.5 rounded-md cursor-pointer border transition-all
-        ${isSelected ? "bg-custom-blue-3 text-white border-custom-blue-4" : "bg-gray-200 text-black border-gray-300"}
+        ${
+          isSelected
+            ? "bg-custom-blue-3 text-white border-custom-blue-4"
+            : "bg-gray-200 text-black border-gray-300"
+        }
         hover:bg-custom-blue-2 hover:text-white`}
     >
       {label}
@@ -16,7 +21,6 @@ const Radio = ({ name, value, handleChange, selectedValue, disable }) => {
         checked={isSelected}
         onChange={handleChange}
         className="hidden"
-        disabled={disable}
       />
     </label>
   );
