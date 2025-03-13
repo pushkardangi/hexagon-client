18c6444973559ad4d4d45eedb68b2fa3d1a71180
Pushkar Dangi <pushkardangi746@gmail.com>
2025-03-13 08:37:45 +0530

    fix: improve error handling in API error response and retry logic

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -8,22 +8,23 @@ export const axiosInstance = axios.create({
 
 export const handleApiError = async (error) => {
   console.log("Error inside handleapierror:", error);
+  console.log("Error response:", error?.response);
   try {
-    const errorMessage = error.response?.data?.message || "An unexpected error occurred.";
-    const status = error.response?.status;
+    const errorMessage = error?.response?.data?.message;
+    const status = error?.response?.status;
 
     // when Access token expired, get new access token and prompt user to try again
     if (status === 401 && (errorMessage === "Access Token Expired!" || errorMessage === "Unauthorized! Access token is missing!")) {
       try {
         await renewTokens();
-        return { error: "Something went wrong. Please try again." };
+        return axiosInstance(error?.config); // retry the original request
       } catch (tokenError) {
-        return { error: "Session expired. Please log in again." };
+        return { error: `Session expired. Please log in again: ${tokenError}` };
       }
     }
 
     return { error: errorMessage };
   } catch (err) {
-    throw new Error("An unexpected error occurred.");
+    throw new Error("An unexpected err occurred inside api error!");
   }
 };
