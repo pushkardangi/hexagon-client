874a8ae53d71db9937fb210a95b80836c52597c6
Pushkar Dangi <pushkardangi746@gmail.com>
2025-03-13 08:37:45 +0530

    fix: improve error handling  and retry logic, improve user retrieval from localStorage, and update end message in gallery to handle empty state

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -7,23 +7,25 @@ export const axiosInstance = axios.create({
 });
 
 export const handleApiError = async (error) => {
-  console.log("Error inside handleapierror:", error);
   try {
-    const errorMessage = error.response?.data?.message || "An unexpected error occurred.";
-    const status = error.response?.status;
+    const errorMessage = error?.response?.data?.message || "An unexpected error occurred.";
+    const status = error?.response?.status;
+
+    console.log("Error occured :", status, errorMessage); // Log
 
     // when Access token expired, get new access token and prompt user to try again
     if (status === 401 && (errorMessage === "Access Token Expired!" || errorMessage === "Unauthorized! Access token is missing!")) {
       try {
         await renewTokens();
-        return { error: "Something went wrong. Please try again." };
+        const result = axiosInstance(error?.config); // retry the original request
+        return result;
       } catch (tokenError) {
-        return { error: "Session expired. Please log in again." };
+        return { error: `Session expired. Please log in again: ${tokenError}` };
       }
     }
 
     return { error: errorMessage };
   } catch (err) {
-    throw new Error("An unexpected error occurred.");
+    throw new Error(`An unexpected err occurred : ${err}`);
   }
 };
